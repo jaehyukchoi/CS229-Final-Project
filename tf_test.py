ee6f1bd95c95799760a49526125d439d6a148ce8
orbancedric <orbancedric94@gmail.com>
2017-11-17 12:20:01 -0800

    idk what

---
+++ tf_test.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x for x in local_device_protos if x.device_type == 'GPU']
+
+print(get_available_gpus())
